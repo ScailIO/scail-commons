4b380e0696c7333af04eaf21464c1f782ca26ada
Marconi Lanna <marconilanna@users.noreply.github.com>
2020-07-02 11:56:00 -0400

    Update dependencies
    Commons Codec 1.14
    Commons Lang 3.10
    Commons Text 1.8
    ICU4J 67.1
    Typesafe Config 1.4.0
    Mockito Scala 1.14.8

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -33,24 +33,24 @@ trait Dependencies {
   )
 
   // Java
-  val commonsCodec      = "commons-codec"               % "commons-codec"           % "1.13"        % Provided
-  val commonsLang       = "org.apache.commons"          % "commons-lang3"           % "3.9"         % Provided
-  val commonsText       = "org.apache.commons"          % "commons-text"            % "1.7"         % Provided
+  val commonsCodec      = "commons-codec"               % "commons-codec"           % "1.14"        % Provided
+  val commonsLang       = "org.apache.commons"          % "commons-lang3"           % "3.10"        % Provided
+  val commonsText       = "org.apache.commons"          % "commons-text"            % "1.8"         % Provided
   val commonsValidator  = "commons-validator"           % "commons-validator"       % "1.6"         % Provided
-  val icu4j             = "com.ibm.icu"                 % "icu4j"                   % "64.2"        % Provided
+  val icu4j             = "com.ibm.icu"                 % "icu4j"                   % "67.1"        % Provided
   val jBCrypt           = "de.svenkubiak"               % "jBCrypt"                 % "0.4.1"       % Provided
   val modeshapeCommon   = "org.modeshape"               % "modeshape-common"        % "5.4.1.Final" % Provided
-  val typesafeConfig    = "com.typesafe"                % "config"                  % "1.3.4"       % Provided
+  val typesafeConfig    = "com.typesafe"                % "config"                  % "1.4.0"       % Provided
 
   // Scala
   val scalaReflect      = "org.scala-lang"              % "scala-reflect"           % v.scala
   val scalaLogging      = "com.typesafe.scala-logging" %% "scala-logging"           % "3.9.2"
 
   // Test
-  val mockitoScala      = "org.mockito"                %% "mockito-scala-scalatest" % "1.5.14"
+  val mockitoScala      = "org.mockito"                %% "mockito-scala-scalatest" % "1.14.8"
   val scalatest         = "org.scalatest"              %% "scalatest-freespec"      % v.scalatest
   val scalatestDiagrams = "org.scalatest"              %% "scalatest-diagrams"      % v.scalatest
-  val slf4jNop          = "org.slf4j"                   % "slf4j-nop"               % "1.7.28"
+  val slf4jNop          = "org.slf4j"                   % "slf4j-nop"               % "1.7.30"
 
   // Compiler plug-ins
   val linter            = "org.psywerx.hairyfotr"      %% "linter"                  % "0.1.17"
